08647994d3a23f0b229854fe2ed35e0a4c05ffda
Nathan White <white.lnathan@gmail.com>
2025-09-06 11:26:28 -0400

    Add Elder Scrolls Online template

---
+++ Source/Games/Elder Scrolls Online/main.cpp
@@ -0,0 +1,178 @@
+// Specify a define with the name of the game here (GAME_*).
+// This is optional but can be used to hardcode custom (per game) behaviours in the core library,
+// or other reasons. We can't automate it through the project define system (based on the project name) because
+// we need these defines to be upper case and space free.
+#define GAME_ELDER_SCROLLS_ONLINE 1
+
+// Define all the global "core" defines before including its files:
+// Enable this to be able to use DLSS's code
+#define ENABLE_NGX 0
+// Update samples to override the bias, based on the rendering resolution etc
+#define UPGRADE_SAMPLERS 0
+#define GEOMETRY_SHADER_SUPPORT 0
+
+// Instead of "manually" including the "core" library, we simply include its main code file (which is a header).
+// The library in itself is standalone, as in, it compiles fine and could directly be used as a template addon etc if built as dll but,
+// there's a major limitation in how libraries dependencies work by nature, and that is that you can only make
+// one version of them for all other projects to use. For performance and tidiness reasons, we are interested in
+// having global defines that can be turned on and off per game, as opposed to runtime (static) parameters.
+// Hence why we specify the global defines before including the core Luma file (where near all of the generic Luma implementation is).
+// If we wanted to use a library, we'd also need to add a core "main" definition in a cpp file, to link it properly.
+// All externs that are currently defined in core would also need to be manually defined in each game's implementation (e.g. see "RESHADE_EXTERNS" above).
+// The only disadvantage of not actually including the core as a library, is that we'll have to add the same include/library dependencies in our
+// game project (e.g. add ReShade, DLSS, etc), and manually add all cpp files too.
+//
+// To compile in different modes (e.g. "DEVELOPMENT", "TEST" etc see "global_defines.h").
+#include "..\..\Core\core.hpp"
+
+struct GameDeviceDataElderScrollsOnline final : public GameDeviceData
+{
+   bool has_drawn_tonemap = false;
+};
+
+class ElderScrollsOnline final : public Game
+{
+   // Optional helper to hide ugly casts
+   static GameDeviceDataElderScrollsOnline& GetGameDeviceData(DeviceData& device_data)
+   {
+      return *static_cast<GameDeviceDataElderScrollsOnline*>(device_data.game);
+   }
+
+public:
+   // You can define any data to initialize once here
+   void OnInit(bool async) override
+   {
+      // You can add shader defines that will end up in the advanced settings for users to modify here.
+		// These will be defined in the shaders, so they can be used to do static branches in them.
+      // Ideally they should also be defined in the game's Settings.hlsl file.
+      std::vector<ShaderDefineData> game_shader_defines_data = {
+         {"TONEMAP_TYPE", '1', false, false, "0 - Vanilla SDR\n1 - Luma HDR (Vanilla+)"},
+      };
+      shader_defines_data.append_range(game_shader_defines_data);
+      assert(shader_defines_data.size() < MAX_SHADER_DEFINES); // Make sure there's room for at least one extra custom define to add for development (this isn't really relevant outside of development)
+
+      // Define these according to the game's original technical details and the mod's implementation (see their declarations for more).
+      GetShaderDefineData(POST_PROCESS_SPACE_TYPE_HASH).SetDefaultValue('0'); // What space are the colors in? Was the swapchain linear (sRGB texture format)? Did we change post processing to store in linear space?
+      GetShaderDefineData(EARLY_DISPLAY_ENCODING_HASH).SetDefaultValue('0'); // Whether we do gamma correction and paper white scaling during post processing or we delay them until the final display composition pass
+      GetShaderDefineData(VANILLA_ENCODING_TYPE_HASH).SetDefaultValue('0'); // What SDR transfer curve was the game using? Most modern games used sRGB in SDR
+      GetShaderDefineData(GAMMA_CORRECTION_TYPE_HASH).SetDefaultValue('1'); // What SDR transfer curve to we want to emulate? This is relevant even if we work in linear space, as there can be a gamma mismatch on it
+      GetShaderDefineData(UI_DRAW_TYPE_HASH).SetDefaultValue('0'); // How does the UI draw in?
+
+      // Customize the cbuffers indexes here. They should be between 0 and 13 ("D3D11_COMMONSHADER_CONSTANT_BUFFER_API_SLOT_COUNT - 1"),
+      // set them to invalid values (e.g. -1) to not set them (they won't be uploaded to the GPU, and thus not usable in shaders). All their values need to be different if they are valid.
+      // These will be automatically sent to the GPU for every shader pass the mod overrides.
+      luma_settings_cbuffer_index = 13;
+      luma_data_cbuffer_index = 12; // Needed for debugging textures and having custom per pass data (including handling the final pass properly).
+      luma_ui_cbuffer_index = -1; // Optional, see "UI_DRAW_TYPE" (this is for type 1)
+
+      // Init the game settings here, you could also load them from ini in "LoadConfigs()".
+      // These can be defined in a header shared between shaders and c++, and are sent to the GPU every time they change.
+      // It should be in "Shaders/Game/Includes/GameCBuffers.hlsl", you can either add that to your Visual Studio project as forced include as some projects do,
+      // or manually add it to the project files and include that at the top (before anything else, as the game cb settings struct needs to be defined), or make a copy of it in c++ (making sure it's mirrored to hlsl),
+      // and define "LUMA_GAME_CB_STRUCTS" to avoid it being re-defined.
+      // Set "device_data.cb_luma_global_settings_dirty" to true when you change them (if you care for them being re-uploaded to the GPU).
+      cb_luma_global_settings.GameSettings.GameSetting01 = 0.5f;
+      cb_luma_global_settings.GameSettings.GameSetting02 = 33;
+   }
+
+   // This needs to be overridden with your own "GameDeviceData" sub-class (destruction is automatically handled)
+   void OnCreateDevice(ID3D11Device* native_device, DeviceData& device_data) override
+   {
+      device_data.game = new GameDeviceDataElderScrollsOnline;
+   }
+
+   bool OnDrawCustom(ID3D11Device* native_device, ID3D11DeviceContext* native_device_context, CommandListData& cmd_list_data, DeviceData& device_data, reshade::api::shader_stage stages, const ShaderHashesList<OneShaderPerPipeline>& original_shader_hashes, bool is_custom_pass, bool& updated_cbuffers) override
+   {
+      auto& game_device_data = GetGameDeviceData(device_data);
+
+      // Random pixel shader hash as an example
+      static uint32_t pixel_shader_hash_tonemap = Shader::Hash_StrToNum("8B2321A2");
+
+      // Here you can track and customize shader passes by hash, you can do whatever you want in it
+      if (!game_device_data.has_drawn_tonemap && original_shader_hashes.Contains(pixel_shader_hash_tonemap, reshade::api::shader_stage::pixel))
+      {
+      game_device_data.has_drawn_tonemap = true;
+
+         device_data.has_drawn_main_post_processing = true;
+      }
+
+      return false; // Don't cancel the original draw call
+   }
+   void OnPresent(ID3D11Device* native_device, DeviceData& device_data) override
+   {
+      auto& game_device_data = GetGameDeviceData(device_data);
+
+      game_device_data.has_drawn_tonemap = false;
+   }
+
+   void PrintImGuiAbout() override
+   {
+      // Remember to credit Luma developers, the game mod creators, and all third party code that is used (plus, optionally, testers too)
+      ImGui::Text("Elder Scrolls Online Luma mod - about and credits section", "");
+   }
+};
+
+// This is where everything starts from, the very first call to the dll.
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
+{
+   if (ul_reason_for_call == DLL_PROCESS_ATTACH)
+   {
+      // Optimization (we don't need DLL_THREAD_ATTACH)
+      DisableThreadLibraryCalls(hModule);
+
+      // Setup the globals (e.g. name etc). It's good to do this stuff before registering the ReShade addon, to make sure the names are up to date.
+      const char* project_name = PROJECT_NAME;
+      const char* cleared_project_name = (project_name[0] == '_') ? (project_name + 1) : project_name; // Remove the potential "_" at the beginning. This can include spaces!
+
+      uint32_t mod_version = 1; // Increase this to reset the game settings and shader binaries after making large changes to your mod
+      Globals::SetGlobals(cleared_project_name, "Elder Scrolls Online Luma mod", nullptr /*E.g. Nexus link*/, mod_version);
+
+      // The following can be toggled in the dev settings (it generally only fully applies after changing the game's resolution).
+      // Default these to "false" for the mod to not do "anything" that might cause issues with the game by default.
+      enable_swapchain_upgrade = true;
+      swapchain_upgrade_type = 1;
+      enable_texture_format_upgrades = true;
+      // Texture upgrades (8 bit unorm and 11 bit float etc to 16 bit float)
+      texture_upgrade_formats = {
+            reshade::api::format::r8g8b8a8_unorm,
+            reshade::api::format::r8g8b8a8_unorm_srgb,
+            reshade::api::format::r8g8b8a8_typeless,
+            reshade::api::format::r8g8b8x8_unorm,
+            reshade::api::format::r8g8b8x8_unorm_srgb,
+            reshade::api::format::b8g8r8a8_unorm,
+            reshade::api::format::b8g8r8a8_unorm_srgb,
+            reshade::api::format::b8g8r8a8_typeless,
+            reshade::api::format::b8g8r8x8_unorm,
+            reshade::api::format::b8g8r8x8_unorm_srgb,
+            reshade::api::format::b8g8r8x8_typeless,
+
+            reshade::api::format::r10g10b10a2_unorm,
+            reshade::api::format::r10g10b10a2_typeless,
+
+            //reshade::api::format::r16g16b16a16_unorm,
+
+            reshade::api::format::r11g11b10_float,
+      };
+      // Set this to the size and type of the main color grading LUT you might want to upgrade (optional)
+      texture_format_upgrades_lut_size = 32;
+      texture_format_upgrades_lut_dimensions = LUTDimensions::_2D;
+		// In case the textures failed to upgrade, tweak the filtering conditions to be more lenient (e.g. aspect ratio checks etc).
+      texture_format_upgrades_2d_size_filters = 0 | (uint32_t)TextureFormatUpgrades2DSizeFilters::SwapchainResolution | (uint32_t)TextureFormatUpgrades2DSizeFilters::SwapchainAspectRatio;
+
+#if DEVELOPMENT // If you want to track any shader names over time, you can hardcode them here by hash (they can be a useful reference in the pipeline)
+      forced_shader_names.emplace(std::stoul("FD2925B4", nullptr, 16), "Tracked Shader Name");
+#endif
+
+#if !DEVELOPMENT // Put shaders that a previous version of the mod used but has ever since been deleted here, so that users updating the mod from an older version won't accidentally load them
+      old_shader_file_names.emplace("Bloom_0xDC9373A8.ps_5_0.hlsl");
+#endif
+
+      // Create your game sub-class instance (it will be automatically destroyed on exit).
+      // You do not need to do this if you have no custom data to store.
+      game = new ElderScrollsOnline();
+   }
+
+   CoreMain(hModule, ul_reason_for_call, lpReserved);
+
+   return TRUE;
+}
